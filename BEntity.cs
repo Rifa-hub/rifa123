13aa2d7b769f4082aa5d51c1637431c919aab9cd
ovizitdasjoy <76483108+ovizitdasjoy@users.noreply.github.com>
2021-01-02 20:39:54 +0600

    Create BEntity.cs

---
+++ BEntity.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petsolutionlogin.Entities
+{
+    public class BEntity
+    {
+        public int Id { get; set; }
+
+        //public string Specialist { get; set; }
+    }
+}
